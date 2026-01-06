0cc3139fea6e6cc77ae006d9e8b63ac4c5f35884
Sangiria <lisaosadchenko@gmail.com>
2026-01-06 13:51:57 +0300

    feat: config struct

--- main.go
+++ main.go
@@ -7,6 +7,67 @@ import (
 	"os"
 )
 
+type LogLevel struct {
+  	LogLevel string `json:"loglevel"`
+}
+
+type Inbound struct {
+	Tag       		string      	`json:"tag"`
+  	Listen     	 	string      	`json:"listen"`
+  	Port      		uint16      	`json:"port"`
+  	Protocol    	string      	`json:"protocol"`
+  	InboundSettings InboundSettings `json:"settings"`
+}
+
+type InboundSettings struct {
+  	Auth  string  	`json:"auth"`
+  	Udp   bool  	`json:"udp"`
+}
+
+type Outbound struct {
+  	Tag            string         `json:"tag"`
+  	Protocol       string         `json:"protocol"`
+  	Settings       VlessSettings  `json:"settings"`
+  	StreamSettings StreamSettings `json:"streamSettings"`
+}
+
+type VlessSettings struct {
+  	VNext []VNext `json:"vnext"`
+}
+
+type VNext struct {
+  	Address string		`json:"address"`
+  	Port    uint16		`json:"port"`
+  	Users   []VlessUser	`json:"users"`
+}
+
+type VlessUser struct {
+  	ID         string `json:"id"`
+  	Encryption string `json:"encryption"`
+  	Flow       string `json:"flow,omitempty"`
+}
+
+type StreamSettings struct {
+  	Network         string          `json:"network"`
+  	Security        string          `json:"security"`
+  	RealitySettings RealitySettings `json:"realitySettings"`
+}
+
+type RealitySettings struct {
+  	ServerName  string `json:"serverName"`
+  	Fingerprint string `json:"fingerprint"`
+  	PublicKey   string `json:"publicKey"`
+  	ShortID     string `json:"shortId"`
+}
+
+type Config struct {
+  	LogLevel    LogLevel	`json:"log"`
+  	Inbounds   []Inbound	`json:"inbounds"`
+  	Outbounds  []Outbound	`json:"outbounds"`
+}
+
+var xrayPath = "bin"
+
 func main() {
 	fmt.Print("Enter url: ")
   	reader := bufio.NewReader(os.Stdin)
@@ -14,6 +75,6 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-	
+
     fmt.Printf("read line: %s\n", line)
 }
