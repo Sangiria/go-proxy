1d1216e1712f83d3aa4784590686e74d3ac774f1
Sangiria <lisaosadchenko@gmail.com>
2026-01-10 20:33:29 +0300

    feat: xhttp support in models

--- core/models/config.go
+++ core/models/config.go
@@ -1,5 +1,7 @@
 package models
 
+import "encoding/json"
+
 type LogLevel struct {
   	LogLevel string `json:"loglevel"`
 }
@@ -10,6 +12,12 @@ type Inbound struct {
   	Port      		uint16      	`json:"port"`
   	Protocol    	string      	`json:"protocol"`
   	InboundSettings InboundSettings `json:"settings"`
+	Sniffing		Sniffing		`json:"sniffing"`
+}
+
+type Sniffing struct {
+	Enabled			bool		`json:"enabled"`
+	DestOverride	[]string	`json:"destOverride"`
 }
 
 type InboundSettings struct {
@@ -18,13 +26,13 @@ type InboundSettings struct {
 }
 
 type Outbound struct {
-  	Tag            string         `json:"tag"`
-  	Protocol       string         `json:"protocol"`
-  	Settings       VlessSettings  `json:"settings"`
-  	StreamSettings StreamSettings `json:"streamSettings"`
+  	Tag            string         		`json:"tag"`
+  	Protocol       string         		`json:"protocol"`
+  	Settings       OutboundSettings  	`json:"settings"`
+  	StreamSettings StreamSettings 		`json:"streamSettings"`
 }
 
-type VlessSettings struct {
+type OutboundSettings struct {
   	VNext []VNext `json:"vnext"`
 }
 
@@ -35,22 +43,32 @@ type VNext struct {
 }
 
 type VlessUser struct {
-  	ID         string `json:"id"`
-  	Encryption string `json:"encryption"`
-  	Flow       string `json:"flow,omitempty"`
+  	ID         	string 	`json:"id"`
+  	Encryption 	string 	`json:"encryption"`
+  	Flow       	string 	`json:"flow,omitempty"` 	//optional
+	Level		uint	`json:"level,omitempty"`	//optional
 }
 
 type StreamSettings struct {
-  	Network         string          `json:"network"`
-  	Security        string          `json:"security"`
-  	RealitySettings RealitySettings `json:"realitySettings"`
+  	Network         string          `json:"network"`					//for now tcp or xhttp only
+  	Security        string          `json:"security,omitempty"`			//for now reality only
+  	RealitySettings RealitySettings `json:"realitySettings,omitzero"`
+	XttpSettings	XttpSettings	`json:"xhttpSettings,omitzero"` 	//if transport is xhttp (tcp is the default)
+}
+
+type XttpSettings struct {
+	Host		string			`json:"host,omitempty"`		//optional
+	Path		string			`json:"path,omitempty"`		//optional
+	Mode		string			`json:"mode,omitempty"`		//optional
+	XttpExtra	json.RawMessage	`json:"extra,omitempty"`	//optional	
 }
 
 type RealitySettings struct {
-  	ServerName  string `json:"serverName"`
-  	Fingerprint string `json:"fingerprint"`
-  	PublicKey   string `json:"publicKey"`
-  	ShortID     string `json:"shortId"`
+  	ServerName  string 	`json:"serverName"` 		//sni
+  	Fingerprint string 	`json:"fingerprint"`		//fp
+  	PublicKey   string 	`json:"publicKey"`			//pbk
+  	ShortID     string 	`json:"shortId,omitempty"`	//sid optional
+	SpiderX		string	`json:"spiderX,omitempty"`	//optional
 }
 
 type Config struct {
