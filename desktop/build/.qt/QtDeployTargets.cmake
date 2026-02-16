4ff25fc6d10dd5993756ac895b5cef47b403228c
sangiriah <lisaosadchenko@gmail.com>
2026-02-16 15:51:35 +0300

    feat (frontend): cmake edit

---
+++ desktop/build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_desktop_FILE /mnt/other/sangiri/study/projects/go-proxy/desktop/build/desktop)
+set(__QT_DEPLOY_TARGET_desktop_TYPE EXECUTABLE)
---
+++ desktop/build/CMakeFiles/desktop_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/desktop_autogen"
+  "desktop_autogen/include/form/ui_addsubscriptiondialog.h"
+  "desktop_autogen/include/form/ui_mainwindow.h"
+  "desktop_autogen/mocs_compilation.cpp"
+  "desktop_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/desktop_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
