5cec1d8be910114048ef49077d88f5f6246e9f17
sangiriah <lisaosadchenko@gmail.com>
2026-02-20 10:57:16 +0300

    feat(backend): FetchVLESSLinks

--- core/links/fetch.go
+++ core/links/fetch.go
@@ -1,7 +1,54 @@
 package links
 
-import "net/url"
+import (
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
 
-func FetchVLESSLinks(u *url.URL) {
+func FetchVLESSLinks(url string) ([]string, error){
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Error making request: %v\n", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Error: Received non-OK status code: %d\n", response.StatusCode)
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading response body: %v\n", err)
+	}
+
+	decoded, err := decodeBase64(string(body))
+	if err != nil {
+		return nil, fmt.Errorf("Invalid body contents: %v\n", err)
+	}
+	links := strings.Split(string(decoded), "\n")
+
+	return links, nil
+}
+
+func decodeBase64(s string) ([]byte, error) {
+	encodings := []*base64.Encoding{
+		base64.StdEncoding,
+		base64.RawStdEncoding,
+		base64.URLEncoding,
+		base64.RawURLEncoding,
+	}
+
+	var err error
+	for _, e := range encodings {
+		var decoded []byte
+		decoded, err = e.DecodeString(s)
+		if err == nil {
+			return decoded, nil
+		}
+	}
 	
+	return nil, err
 }
--- core/models/state.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type State struct {
-	ActiveNodeId	string						`json:"active_node"`
-	Subscriptions	map[string]*Subscription	`json:"subscriptions"`
-	Nodes			map[string]*Node			`json:"nodes"`
-}
