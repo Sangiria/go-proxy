108eb245859d7ff4a9c30d78af0d00ce8bd7fbe2
sangiriah <lisaosadchenko@gmail.com>
2026-03-12 12:21:52 +0300

    feat(frontend): empty string detection

--- desktop/main.py
+++ desktop/main.py
@@ -1,17 +1,14 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
-
-from form.mainwindow import Ui_MainWindow
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
+from PyQt6.QtWidgets import QApplication
+from logic.window import MainWindow
+from service.worker import channel
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec())
+
+    exit_code = app.exec()
+    channel.close()
+
+    sys.exit(exit_code)
---
+++ desktop/service/worker.py
@@ -0,0 +1,22 @@
+from PyQt6.QtCore import QThread, pyqtSignal
+from generated import proxy_pb2_grpc
+import grpc
+
+channel = grpc.insecure_channel('localhost:50051')
+stub = proxy_pb2_grpc.NodeServiceStub(channel)
+
+class GrpcWorker(QThread):
+    finished = pyqtSignal(object)
+    error = pyqtSignal(str)
+
+    def __init__(self, method, *args):
+        super().__init__()
+        self.method = method
+        self.args = args
+
+    def run(self):
+        try:
+            response = self.method(*self.args)
+            self.finished.emit(response)
+        except Exception as e:
+            self.error.emit(str(e))
